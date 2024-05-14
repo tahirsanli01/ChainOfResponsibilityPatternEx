e27dd8dc384ce0c6c1cdd09fe2a720ce4f6266d7
SA TECH YAZILIM <98909642+satechyazilim@users.noreply.github.com>
2024-05-14 18:28:48 +0300

    Commit

--- ChainOfResponsibilityPatternEx/Program.cs
+++ ChainOfResponsibilityPatternEx/Program.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,24 +16,111 @@ namespace ChainOfResponsibilityPatternEx
     {
         static void Main(string[] args)
         {
-            // Create the request handlers
-            var authenticationHandler = new AuthenticationHandler();
-            var authorizationHandler = new AuthorizationHandler();
-            var validationHandler = new ValidationHandler();
+            //var authenticationHandler = new GrayScaleHandler();
+            //var authorizationHandler = new MonoChromeHandler();
+            //var validationHandler = new ValidationHandler();
 
-            // Set the chain of responsibility
-            authenticationHandler.SetNextHandler(authorizationHandler);
-            authorizationHandler.SetNextHandler(validationHandler);
+            //authenticationHandler.SetNextHandler(authorizationHandler);
+            //authorizationHandler.SetNextHandler(validationHandler);
 
-            // Process the request
-            authenticationHandler.HandleRequest();
+
+
+            //authenticationHandler.InitializeFirstHandler(new Bitmap(""));
+            //var result = authenticationHandler.HandleRequest();
+
+            // JSON'dan veriyi oku
+            string json = File.ReadAllText("C:\\Users\\sanli\\source\\repos\\WORKING\\ChainOfResponsibilityPatternEx\\ChainOfResponsibilityPatternEx\\json1.json");
+            var handlerConfigs = JsonConvert.DeserializeObject<List<HandlerConfig>>(json);
+
+            // Handler'ları oluştur ve zincirle
+            IRequestHandler firstHandler = null;
+            IRequestHandler prevHandler = null;
+            foreach (var config in handlerConfigs)
+            {
+                var handler = CreateHandler(config.HandlerType);
+                handler.InitializeFirstHandler(config.Data);
+                if (prevHandler != null)
+                {
+                    prevHandler.SetNextHandler(handler);
+                }
+                else
+                {
+                    firstHandler = handler;
+                }
+                prevHandler = handler;
+            }
+
+            // İstekleri işle
+            firstHandler.HandleRequest();
+            Console.ReadLine();
+        }
+
+
+        private static IRequestHandler CreateHandler(string handlerType)
+        {
+            // "ChainOfResponsibilityPatternEx" isim alanını içeren derleme yükleniyor
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // Tip adı "handlerType" olan sınıfı ara
+            Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == handlerType);
+
+            if (type != null && typeof(IRequestHandler).IsAssignableFrom(type))
+            {
+                // Tip IRequestHandler arayüzünü uyguluyorsa bir örnek oluştur
+                return (IRequestHandler)Activator.CreateInstance(type);
+            }
+            else
+            {
+                // Belirtilen türde bir sınıf bulunamadı veya IRequestHandler arayüzünü uygulamıyorsa null döndür
+                return null;
+            }
         }
     }
 
 
 
+    public class GrayScaleHandler : RequestHandlerBase
+    {
+        [Config("GrayScaleHandlerData")]
+        protected override void ProcessRequest(object data)
+        {
+            Console.WriteLine("GrayScale handler processing request");
+            PutNextHandlerData("AuthorizationHandlerData");
+        }
+    }
 
 
+    public class MonoChromeHandler : RequestHandlerBase
+    {
+        [Config("MonoChromeHandlerData")]
+        protected override void ProcessRequest(object data)
+        {
+            Console.WriteLine("MonoChrome handler processing request");
+            PutNextHandlerData("ValidationHandlerData");
+        }
+    }
+
+    public class ValidationHandler : RequestHandlerBase
+    {
+        [Config("ValidationHandlerData")]
+        protected override void ProcessRequest(object data)
+        {
+            Console.WriteLine("Validation handler processing request");
+            PutNextHandlerData("Finish");
+        }
+    }
+
+
+    internal class HandlerConfig
+    {
+        public string HandlerType { get; set; }
+        public object Data { get; set; }
+    }
+
+
+
+
+    #region ConfigClass
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     public class Config : Attribute
     {
@@ -39,80 +131,239 @@ namespace ChainOfResponsibilityPatternEx
         }
     }
 
-
-
-
-
-
-    // Handler interface
     public interface IRequestHandler
     {
         void SetNextHandler(IRequestHandler handler);
-        void HandleRequest();
+        object HandleRequest();
+        void InitializeFirstHandler(object value);
     }
 
-    // Abstract base class for concrete handlers
     public abstract class RequestHandlerBase : IRequestHandler
     {
-        Dictionary<string, object> _data = new Dictionary<string, object>();
+        private static Dictionary<string, object> _data = new Dictionary<string, object>();
         private IRequestHandler _nextHandler;
+
+        public void InitializeFirstHandler(object value)
+        {
+            var handlerName = this.GetType().Name;
+            _data[$"{handlerName}Data"] = value;
+        }
 
         public void SetNextHandler(IRequestHandler handler)
         {
             _nextHandler = handler;
         }
 
-        public virtual void HandleRequest()
+        private void PutData(object value)
         {
-            Type methodNextAttribute = this.GetType();
-            var x = methodNextAttribute.GetNestedTypes();
-            var result = methodNextAttribute.GetRuntimeMethods();
-            var xc = result.ToList()[0];
-            var result2 = xc.GetCustomAttribute<Config>();
+            string handlerName = "";
+            if (_nextHandler != null)
+            {
+                handlerName = _nextHandler.GetType().Name;
+            }
+            else
+            {
+                handlerName = "Finish";
+            }
 
-            ProcessRequest(_data[result2._name]);
+            _data[$"{handlerName}Data"] = value;
+        }
+
+        public void PutNextHandlerData(object value)
+        {
+            PutData(value);
+        }
+
+        private object GetData(string name)
+        {
+            return _data[name];
+        }
+
+        public virtual object HandleRequest()
+        {
+            MethodInfo methodNextAttribute = this.GetType().GetRuntimeMethods().ToList()[0];
+            var methodAttribute = methodNextAttribute.GetCustomAttribute<Config>();
+
+            ProcessRequest(GetData(methodAttribute._name));
 
             if (_nextHandler != null)
             {
                 _nextHandler.HandleRequest();
             }
+
+            return GetData("FinishData");
         }
 
         protected abstract void ProcessRequest(object name);
     }
-
-
-
-    // Concrete handler: Authentication Handler
-    public class AuthenticationHandler : RequestHandlerBase
-    {
-        [Config("AuthenticationHandlerData")]
-        protected override void ProcessRequest(object data)
-        {
-            Console.WriteLine("Authentication handler processing request");
-            // Perform authentication logic
-        }
-    }
-
-    // Concrete handler: Authorization Handler
-    public class AuthorizationHandler : RequestHandlerBase
-    {
-        [Config("AuthorizationHandlerData")]
-        protected override void ProcessRequest(object data)
-        {
-            Console.WriteLine("Authorization handler processing request");
-            // Perform authorization logic
-        }
-    }
-
-    // Concrete handler: Validation Handler
-    public class ValidationHandler : RequestHandlerBase
-    {
-        [Config("ValidationHandlerData")]
-        protected override void ProcessRequest(object data)
-        {
-            Console.WriteLine("Validation handler processing request");
-            // Perform validation logic
-        }
-    }
+    #endregion
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using Newtonsoft.Json;
+
+//namespace ChainOfResponsibilityPatternEx
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // JSON'dan veriyi oku
+//            string json = File.ReadAllText("C:\\Users\\sanli\\source\\repos\\WORKING\\ChainOfResponsibilityPatternEx\\ChainOfResponsibilityPatternEx\\json1.json");
+//            var handlerConfigs = JsonConvert.DeserializeObject<List<HandlerConfig>>(json);
+
+//            // Handler'ları oluştur ve zincirle
+//            IRequestHandler firstHandler = null;
+//            IRequestHandler prevHandler = null;
+//            foreach (var config in handlerConfigs)
+//            {
+//                var handler = CreateHandler(config.HandlerType);
+//                handler.InitializeFirstHandler(config.Data);
+//                if (prevHandler != null)
+//                {
+//                    prevHandler.SetNextHandler(handler);
+//                }
+//                else
+//                {
+//                    firstHandler = handler;
+//                }
+//                prevHandler = handler;
+//            }
+
+//            // İstekleri işle
+//            var requestProcessor = new RequestProcessor(firstHandler);
+//            requestProcessor.ProcessRequest("Some request data");
+
+//            Console.ReadLine();
+//        }
+
+//        private static IRequestHandler CreateHandler(string handlerType)
+//        {
+//            // "ChainOfResponsibilityPatternEx" isim alanını içeren derleme yükleniyor
+//            Assembly assembly = Assembly.GetExecutingAssembly();
+
+//            // Tip adı "handlerType" olan sınıfı ara
+//            Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == handlerType);
+
+//            if (type != null && typeof(IRequestHandler).IsAssignableFrom(type))
+//            {
+//                // Tip IRequestHandler arayüzünü uyguluyorsa bir örnek oluştur
+//                return (IRequestHandler)Activator.CreateInstance(type);
+//            }
+//            else
+//            {
+//                // Belirtilen türde bir sınıf bulunamadı veya IRequestHandler arayüzünü uygulamıyorsa null döndür
+//                return null;
+//            }
+//        }
+//    }
+
+
+
+
+//    public class GrayScaleHandler : ConcreteHandler
+//    {
+//        [Config("GrayScaleHandlerData")]
+//        protected override void ProcessRequest(object data)
+//        {
+//            PutNextHandlerData("AuthorizationHandlerData");
+//        }
+//    }
+
+
+//    public class MonoChromeHandler : ConcreteHandler
+//    {
+//        [Config("MonoChromeHandlerData")]
+//        protected override void ProcessRequest(object data)
+//        {
+//            Console.WriteLine("MonoChrome handler processing request");
+//            PutNextHandlerData("ValidationHandlerData");
+//        }
+//    }
+
+//    public class ValidationHandler : ConcreteHandler
+//    {
+//        [Config("ValidationHandlerData")]
+//        protected override void ProcessRequest(object data)
+//        {
+//            Console.WriteLine("Validation handler processing request");
+//            PutNextHandlerData("Finish");
+//        }
+//    }
+
+
+
+
+
+
+
+
+//    internal class HandlerConfig
+//    {
+//        public string HandlerType { get; set; }
+//        public object Data { get; set; }
+//    }
+
+//    public interface IRequestHandler
+//    {
+//        void InitializeFirstHandler(object data);
+//        void SetNextHandler(IRequestHandler handler);
+//        void HandleRequest(string requestData);
+//    }
+
+//    public class ConcreteHandler : IRequestHandler
+//    {
+//        private IRequestHandler _nextHandler;
+
+//        public void InitializeFirstHandler(object data)
+//        {
+//            // İlk handler'ın başlatılması için gerekli işlemler
+//        }
+
+//        public void SetNextHandler(IRequestHandler handler)
+//        {
+//            _nextHandler = handler;
+//        }
+
+//        public void HandleRequest(string requestData)
+//        {
+//            // İsteği işleme süreci
+//            Console.WriteLine("Handling request: " + requestData);
+//            if (_nextHandler != null)
+//            {
+//                _nextHandler.HandleRequest(requestData);
+//            }
+//        }
+//    }
+
+//    internal class RequestProcessor
+//    {
+//        private readonly IRequestHandler _firstHandler;
+
+//        public RequestProcessor(IRequestHandler firstHandler)
+//        {
+//            _firstHandler = firstHandler;
+//        }
+
+//        public void ProcessRequest(string requestData)
+//        {
+//            _firstHandler.HandleRequest(requestData);
+//        }
+//    }
+//}
